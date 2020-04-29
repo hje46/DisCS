fd018c1d1a24f27adbc09cdbe1880bf632c73937
Oliver S <62803006+Simmotipo@users.noreply.github.com>
2020-04-29 15:30:39 +0100

    playbackEngine Updated for Collaboration

--- playbackEngine.h
+++ playbackEngine.h
@@ -3,9 +3,7 @@
 #define speaker 10 // pin no. for speaker
 
 
-void playNote(unsigned int note, int len) {
-  tone(speaker, note, len);
-}
+//Lawrence's playNote void here (replacement for Oliver's placeholder void)
 
 void playTone(int buzzer, unsigned int note, int len) {
   tone(buzzer, note, len);
@@ -18,11 +16,16 @@ void playSong(int startPos) {
   int len = sizeof song / sizeof song[0];
   Serial.println(len);
   while (pointerLoc < len) {
-    if (song[pointerLoc][0] == "") { break; } else {
-      playTone(speaker, song[pointerLoc][0], song[pointerLoc][1]);
-      delay(song[pointerLoc][1]);
-      delay(10);
-      pointerLoc = pointerLoc + 1;
+    int[] line = {};
+    line[0] = song[pointerLoc][0];
+    line[1] = song[pointerLoc][1];
+    line[2] = song[pointerLoc][2];
+    if (line[0] == "") { break; } else {
+      if (line[0] == 1) {
+        //Lawrence's other bit here
+      } else {
+        //Harry's stuff here
+      }
     }
   }
   pointerLoc = oldLoc;
