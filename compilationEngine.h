12c2d135584afa3eb704ef3401cb9b3736c93055
Oliver S <62803006+Simmotipo@users.noreply.github.com>
2020-04-29 17:31:55 +0100

    compilationEngine.h Update
    Updated compulationEngine such that addNote(){} now adds "0" at song[pointerLoc][0] (for note control), and incremented other additions to [1] and [2].
    
    Created addControl(){} void for addition of looping/ending/control instructions. Parity checks to be added later.

--- compilationEngine.h
+++ compilationEngine.h
@@ -3,8 +3,19 @@ int pointerLoc = 0;
 
 void addNote(unsigned int note, int len) {
   Serial.println(pointerLoc);
-  song[pointerLoc][0] = note;
-  song[pointerLoc][1] = len;
-  Serial.println(song[pointerLoc][0] + " " + song[pointerLoc][1]);
+  song[pointerLoc][0] = 0;
+  song[pointerLoc][1] = note;
+  song[pointerLoc][2] = len;
+  Serial.println(song[pointerLoc][0] + " " + song[pointerLoc][1] + " " + song[pointerLoc][2]);
+  pointerLoc = pointerLoc + 1;
+}
+
+void addControl(int code, int arg1, int arg2) {
+ Serial.println(pointerLoc);
+  //Implement parity checks here
+  song[pointerLoc][0] = 0;
+  song[pointerLoc][1] = arg1;
+  song[pointerLoc][2] = arg2;
+  Serial.println(song[pointerLoc][0] + " " + song[pointerLoc][1] + " " + song[pointerLoc][2]);
   pointerLoc = pointerLoc + 1;
 }
